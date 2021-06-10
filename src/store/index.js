f79920420baacb1299f468b09735a370fdf24985
Augusto <juanaugustolombino@protonmail.ch>
2021-06-10 01:24:30 -0300

    Add user and notes to Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,45 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      username: null,
+      password: null,
+    },
+    notes: []
   },
+  getters: {
+    notesCount: (state) => state.posts.length,
+  },
+  // Always synchronous,
+  // is allowed to change the state directly,
+  // responsible for changing only a single piece of data.
   mutations: {
+    INSERT_NOTE(state, note) {
+      state.notes.push(note)
+    },
+    REMOVE_NOTE(state, note) {
+      const index = state.notes.indexOf(note)
+      if (index > -1)
+        state.notes.splice(index, 1)
+    }
   },
+  // Can be synchronous or asynchronous,
+  // they shouldn't change the state directly but call the mutations
   actions: {
+    async insertNote({commit}, payload) {
+      try {
+        commit('INSERT_POST', payload)
+      } catch(error) {
+        //
+      }
+    },
+    async removeNote({commit}, payload) {
+      try {
+        commit('REMOVE_NOTE', payload)
+      } catch(error) {
+        //
+      }
+    }
   },
   modules: {
   }
